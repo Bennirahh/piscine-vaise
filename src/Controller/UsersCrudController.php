1eea7ce089a929f9cdaf6541d5cafaa2e6fcd378
Bennirahh <bennirahdev@gmail.com>
2024-12-02 20:26:02 +0100

    back profil-edit 100% OK faire le style et relier le back a profil classique

--- src/Controller/UsersCrudController.php
+++ src/Controller/UsersCrudController.php
@@ -4,45 +4,62 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Form\UsersType;
-use App\Repository\UsersRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/users/crud')]
 final class UsersCrudController extends AbstractController
 {
     #[Route('/edit', name: 'app_users_crud_edit', methods: ['GET', 'POST'])]
-public function edit(Request $request, EntityManagerInterface $entityManager): Response
-{
-    $user = $this->getUser();
+    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // Récupère l'utilisateur connecté
+        $user = $this->getUser();
 
-    if (!$user) {
-        throw $this->createAccessDeniedException('Vous devez être connecté pour modifier votre profil.');
+        // Si l'utilisateur n'est pas connecté, on l'empêche de modifier son profil
+        if (!$user) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour modifier votre profil.');
+        }
+
+        // Crée le formulaire avec les données de l'utilisateur connecté
+        $form = $this->createForm(UsersType::class, $user);
+
+        // Gère la soumission du formulaire
+        $form->handleRequest($request);
+
+        // Si le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Vérifie si des modifications ont été apportées
+            if ($user !== $this->getUser()) {
+                $this->addFlash('error', 'Les informations n\'ont pas pu être mises à jour.');
+            } else {
+                // Si le formulaire est valide, on persiste les modifications dans la base
+                $entityManager->flush(); // Persiste les modifications en base de données
+
+                // Affiche un message de succès
+                $this->addFlash('success', 'Profil mis à jour avec succès.');
+
+                // Redirige l'utilisateur vers une autre page après la mise à jour
+                return $this->redirectToRoute('profil');
+            }
+        }
+
+        // Rendu du formulaire dans une vue
+        return $this->render('users_crud/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
-    $form = $this->createForm(UsersType::class, $user);
-    $form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-        $entityManager->flush();
-
-        return $this->redirectToRoute('app_users_crud_index', [], Response::HTTP_SEE_OTHER);
+    #[Route('/{id}', name: 'app_users_crud_show', methods: ['GET'])]
+    public function show(Users $user): Response
+    {
+        // Rendu d'une vue pour afficher le profil de l'utilisateur
+        return $this->render('users_crud/show.html.twig', [
+            'user' => $user,
+        ]);
     }
-
-    return $this->render('users_crud/edit.html.twig', [
-        'form' => $form,
-    ]);
-}
-
-#[Route('/{id}', name: 'app_users_crud_show', methods: ['GET'])]
-public function show(Users $user): Response
-{
-    return $this->render('users_crud/show.html.twig', [
-        'user' => $user,
-    ]);
-}
-
 }
--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -2,47 +2,45 @@
 
 namespace App\Form;
 
-use App\Entity\Role;
-use App\Entity\Sector;
 use App\Entity\Users;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TelType;
-use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 
 class UsersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('UserName', TextType::class, [
-            'label' => 'Nom',
-            'attr' => ['class' => 'input-class'], // Ajoute ici des classes CSS si nécessaire
-        ])
-        ->add('UserFirstname', TextType::class, [
-            'label' => 'Prénom',
-            'attr' => ['class' => 'input-class'],
-        ])
-        ->add('email', EmailType::class, [
-            'label' => 'E-mail',
-            'attr' => ['class' => 'input-class'],
-        ])
-        ->add('UserPhone', TelType::class, [
-            'label' => 'Téléphone',
-            'attr' => ['class' => 'input-class'],
-        ])
-        ->add('UserBirthday', BirthdayType::class, [
-            'label' => 'Date de naissance',
-            'widget' => 'single_text',
-            'attr' => ['class' => 'input-class'],
-        ]);
-}
-    
+            ->add('UserName', TextType::class, [
+                'label' => 'Nom d\'utilisateur'
+            ])
+            ->add('UserFirstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email'
+            ])
+            ->add('UserPhone', IntegerType::class, [
+                'label' => 'Numéro de téléphone'
+            ])
+            ->add('UserBirthday', BirthdayType::class, [
+                'label' => 'Date de naissance'
+            ])
+            ->add('UserIsAdmin', CheckboxType::class, [
+                'label' => 'Est administrateur',
+                'required' => false
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Mettre à jour le profil'
+            ]);
+    }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
